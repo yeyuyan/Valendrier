b38d75c12d4c0b7f3824f75b888bbaf5e1d4ea7f
yeyuyan <yuyan.ye677095@gmail.com>
2018-04-19 14:48:54 -0700

    28/03 test

--- app/src/main/java/com/example/yuyan/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/yuyan/myapplication/Main2Activity.java
@@ -10,7 +10,13 @@ import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Color;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.LinearLayout;
 
 import java.util.Calendar;
 
@@ -29,20 +35,20 @@ public class Main2Activity extends AppCompatActivity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
         cal=Calendar.getInstance();
         year=cal.get(Calendar.YEAR);
         month=cal.get(Calendar.MONTH)+1;
         day=cal.get(Calendar.DAY_OF_MONTH);
-
+        myView view=new myView(this);
 
         setTitle(year+"-"+month+"-"+day);
-
-       // datePicker=findViewById(R.id.datePicker);
 
 
         buttonDate=findViewById(R.id.buttonDate);
         buttonDate.setOnClickListener(this);
+        LinearLayout linearLayout=new LinearLayout(this);
+        linearLayout=findViewById(R.id.a10001);
+        linearLayout.addView(view);
     }
 
     @Override
@@ -58,12 +64,46 @@ public class Main2Activity extends AppCompatActivity implements OnClickListener{
         }, year, cal.get(Calendar.MONTH), day).show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-//        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-//                setTitle(year+"-"+month+"-"+day);
-//            }
-//        },year,cal.get(Calendar.MONTH),day).show();
+    class myView extends View {
+        Paint mPaint = new Paint();
+        public myView(Context context) {
+            super(context);
+            mPaint.setColor(Color.YELLOW);
+            mPaint.setStrokeJoin(Paint.Join.ROUND);
+            mPaint.setStrokeCap(Paint.Cap.ROUND);
+            mPaint.setStrokeWidth(3);
+        }
+
+        //实现View中的OnMeasure方法进行测量
+        private double height;
+        private double width;
+        private double sectionHeight;
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            height = MeasureSpec.getSize(heightMeasureSpec);
+            width = MeasureSpec.getSize(widthMeasureSpec);
+            sectionHeight = height / 11;//10hours + 1 title
+            setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
+
+            //setMeasuredDimension(width,height);
+        }
+
+        protected void onDraw(Canvas canvas) {
+
+            //画横线
+            for (int i = 0; i <= 11; i++) {
+                canvas.drawLine(0, (float) (i * sectionHeight),
+                        (float) width, (float) (i * sectionHeight), mPaint);
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/yuyan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yuyan/myapplication/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button=findViewById(R.id.button);
         button.setOnClickListener(this);
     }
